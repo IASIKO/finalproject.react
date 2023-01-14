2ea93ef3a94886a83bb693dbca9a96823383821f
IASIKO <103432601+IASIKO@users.noreply.github.com>
2023-01-14 21:48:49 +0400

    add

--- src/components/product/categoryProducts/index.js
+++ src/components/product/categoryProducts/index.js
@@ -1,17 +1,32 @@
-import { Box } from '@mui/material'
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { useParams } from 'react-router-dom'
-import { fetchCategoryProducts } from '../../../redux'
-import { CategoryProductList } from './CategoryProductList'
+import { Box } from "@mui/material";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { useQueryParam } from "../../../application";
+import { fetchCategoryProducts, useCategoryProducts } from "../../../redux";
+import { CategoryProductList } from "./CategoryProductList";
+import { Paginate } from "./Paginate";
 
 export const CategoryProducts = () => {
-    const dispatch = useDispatch()
-    const {categoryName} = useParams()
-    useEffect(() => {
-        dispatch(fetchCategoryProducts(`${categoryName}?page=1&size=20&sort=name,asc`))
-    }, [categoryName])
-  return  <Box>
-    <CategoryProductList/>
-  </Box> 
-}
+  const dispatch = useDispatch();
+  const { categoryName } = useParams();
+  const { value: page, changeQueryValue: changePage } = useQueryParam("page");
+  const { value: sort } = useQueryParam("sort");
+  const categoryProducts = useCategoryProducts();
+  useEffect(() => {
+    dispatch(
+      fetchCategoryProducts(`${categoryName}?page=1&size=2&sort=name,asc`)
+    );
+  }, [categoryName]);
+  return (
+    <Box>
+      <CategoryProductList />
+      <Paginate
+        currentPage={page}
+        totalPages={categoryProducts.totalPages}
+        changePage={changePage}
+        queryKey="page"
+      />
+    </Box>
+  );
+};
